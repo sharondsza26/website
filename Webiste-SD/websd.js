08c82b0cffcc2957f1b720532b41718df660daf7
sharondsza26 <sharondsouza26@gmail.com>
2021-09-20 04:15:46 +0530

    JavaScript File

--- Webiste-SD/websd.js
+++ Webiste-SD/websd.js
@@ -8,3 +8,25 @@ var currentScrollPos = window.pageYOffset;
   }
   Scrollpos = currentScrollPos;
 };
+
+function changeImageAndText1(){
+  
+  document.getElementById("projDescript").innerHTML="Java-Mini Music Player : This simulation of a music player displays graphics simultaneously with the sound of music. Makes use of conepts in AWT, Swing, Sound, EventListener, MidiEvent, sequencer, etc.";
+  img = document.getElementById("projPic").src="proj1.png";
+  img.style.transform = scale(1);
+
+}
+
+function changeImageAndText2(){
+  
+  document.getElementById("projDescript").innerHTML="Python-Vartalap : Along with the basic concepts of a concurrent server, I built a Message Server that uses a Queue for accepting Client messages and also uses Daemon Threads. It makes use of SEDA Arcitechture.";
+  document.getElementById("projPic").src="proj2.png";
+
+}
+
+function changeImageAndText3(){
+  
+  document.getElementById("projDescript").innerHTML="MachineLearning-FoodDeliveryTimePrediction : Using machine learning concepts I developed a time prediction system along with GoogleAPI for better accuracy. It also contains a pickle model.";
+  document.getElementById("projPic").src="proj3.png";
+
+}
